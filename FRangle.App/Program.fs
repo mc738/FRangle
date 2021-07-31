d04b951fb80b148e9636d1b341ae0acec37dd38b
Max Clifford <me@mclifford.dev>
2021-07-31 22:04:10 +0100

    Added missing bits

---
+++ FRangle.App/Program.fs
@@ -0,0 +1,107 @@
+﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
+
+open System
+open System.Net.Http
+open FRangle.Core
+open FRangle.Core.Domain
+open FRangle.Core.Domain.Errors
+open FRangle.Core.Pipelines
+
+// Define a function to construct a message to print
+let from whom = sprintf "from %s" whom
+
+
+let printResult result =
+    match result with
+    | Ok v -> printfn $"%A{v}" 
+    | Error e -> printfn $"%A{e}"
+
+let runAndDiscard =
+    Processes.run
+    >-> printResult
+    >=> Tools.discard
+
+let buildFRangleTest =
+    Tools.create (fun _ -> [ "C:\\Test\\App"; "C:\\Test\\Core" ]) 
+    >=> Tools.mkdir
+    >=> Tools.create (fun _ -> { Name = "C:\\Program Files\\dotnet\\dotnet"; Args = "build -o C:\\Test\\Core"; StartDirectory = Some "C:\\Users\\44748\\Projects\\FRangle\\FRangle.Core" } : Processes.ProcessParameters)
+    >=> runAndDiscard
+    >=> Tools.create (fun _ -> { Name = "C:\\Program Files\\dotnet\\dotnet"; Args = "build -o C:\\Test\\App"; StartDirectory = Some "C:\\Users\\44748\\Projects\\FRangle\\FRangle.App" } : Processes.ProcessParameters)
+    >=> runAndDiscard
+    >=> Tools.create (fun _ -> "Pipeline complete!")
+    
+let getDotNetGitIgnore _ =    
+    let timestamp = DateTime.UtcNow.ToString("yyyyMMddHHmmss")
+    use client = new HttpClient()
+    
+    // inner function used to stop client from disposing of itself    
+    let pipeline =
+        Tools.create (fun _ -> ()) // A bit of a hack, start was being a pain.
+        >=> Http.getString "https://raw.githubusercontent.com/dotnet/core/main/.gitignore" client
+        >-> printResult
+        >=> Files.write $"C:\\Users\\44748\\Projects\\__Resources\\dot-net-git-ignore_{timestamp}.txt"
+        >=> Tools.create (fun _ -> ".net git ignore saved!")
+        //>=> Http.getString "https://raw.githubusercontent.com/dotnet/core/main/.gitignore" client
+        //>=> Tools.create (fun _ -> "Pipeline complete!")
+    
+    pipeline ()
+    
+let stub =
+    Tools.create (fun _ -> "Stub pipeline.")
+    >-> printResult
+    
+/// An example pipeline collection.
+let pipelines = PipelineCollection<unit, string>.Create([
+    "get-git-ignore", getDotNetGitIgnore
+    "build-FRangle", buildFRangleTest
+    "stub", stub
+]) 
+   
+[<EntryPoint>]
+let main argv =
+
+    //match buildFRangleTest () with
+    //| Ok msg -> printfn $"Success. {msg}"
+    //| Error e -> printfn $"Error! %A{e}"
+
+    
+    match pipelines.Run("stub", ()) with
+    | Ok msg -> printfn $"Success. {msg}"
+    | Error e -> printfn $"Error! %A{e}"
+    
+    (*
+    let test =
+        //Pipelines.start
+        Tools.create (fun _ -> "")
+        >=> Tools.transform (fun s -> 1)
+        >=> Tools.check
+                (fun v ->
+                    if v = 2 then
+                        Ok()
+                    else
+                        Error $"Value is not {2}")
+        >-> (fun r ->
+                match r with
+                | Ok _ -> printfn "All is good!"
+                | Error e -> printfn "Error: %A" e)
+        >?> (fun _ -> printfn "Fixing error..."; Ok 2)
+        >=> Tools.check
+                (fun v ->
+                    if v = 2 then
+                        Ok()
+                    else
+                        Error $"Value is not {2}")
+        >-> (fun r ->
+                match r with
+                | Ok _ -> printfn "All is good!"
+                | Error e -> printfn "Error: %A" e)
+    *)  
+        
+        
+    //match test () with
+    //| Ok r -> printfn "Success! %A" r
+    //| Error e -> printfn "Error: %A" e
+
+    let message = from "F#" // Call the function
+    printfn "Hello world %s" message
+    0 // return an integer exit code
---
+++ README.md
@@ -0,0 +1,8 @@
+# FRangle
+
+`FRangle` is a library for creating pipelines.
+It comes with a set of operations for common tasks like calling HTTP endpoints, IO and ETL functions.
+The aim is to make it easy to chain these operations (and user defined ones) together.
+This following the `unix` philosophy and is heavily influenced by `railway orientated programming`.
+
+The library also contains a collection of `infixes` to cut down on boilplate code. 
